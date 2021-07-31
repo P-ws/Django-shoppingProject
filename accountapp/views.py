a10b986baed67320bc7cd1ae5c113f78a3f75e6a
P-ws <pws12232002@gmail.com>
2021-07-31 18:25:10 +0900

    <model에 저장된 데이터를 받아 출력해주기> 1. form안에 text적는 input생성 적혀진 애들이름도 설정 2. view로 돌아와 text에 적어 이름을 설정해주었던 애들을 변수로 저장 3. 모델을 불러와 변수로 설정후 인풋했던애들을 모델에 텍스트에 넣은후 데이터베이스 저장 4. post로 받았을때 이제 database에서 받은 애들로 값을 내줌.

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,9 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
+
+
 def base(requset):
     # get, post method 설정
     if requset.method == "POST":
-        return render(requset, 'accountapp/middle.html', context={'text':'POST METHOD!!'})
+        # POST라는 메서드에서 괄호안에 있는 이름을 가진애를 가져와라.
+        temp = requset.POST.get('hello_world_input')
+        # HelloWorld는 우리가 만든 모델을 변수
+        # 그리고 form에 text를 적어 불러오는 temp라는 애를 모델안에 설정한 변수 text에 넣어줌
+        # 바로 데이터 베이스에 저장
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        # 저장된 데이터객체를 내보내주는역할
+        return render(requset, 'accountapp/middle.html', context={'hello_world_output':new_hello_world})
     else:
         return render(requset, 'accountapp/middle.html', context={'text': 'GET METHOD!!'})
