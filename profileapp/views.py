07eb48185ff753c27f46bc7d5a9d974156a255e8
P-ws <pws12232002@gmail.com>
2021-08-01 20:53:27 +0900

    <profile의 이미지,문구,닉네임을 보이게하고, form에 관한애들을 유져와 매칭 시키기> 1. 프로필의 createview 만들어주기 2. 만들어준 form들은 있지만 user랑 연결되어있지않아 함수로 해당 form들을 user와 임시로 매칭시켜줌 3. view함수 만들었으니까 url설정 해주기 4. createview의 템플릿에 맞게 템플릿 만들기 create.html(주의 이미지를 받아와야하기때문에 enctype을 설정해주어야함) 5. profile도 마찬가지로 mypage들어갔을때 보여야함으로 detail에서 유저의 프로필이있다면 유저의 아이디말고 닉네임이 보이도록, 프로필이없다면 프로필을 만드는 링크 나오게 설정

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
 
 
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:base')
+    template_name = 'profileapp/create.html'
+
+    #form_valid의 form은 forms의 애들을 받아오는거임(이것을 해주는 이유는 다른사람이 나의 이미지, 문구등을 바꿀수 있기때문에user을 매칭 시켜줌)
+    def form_valid(self, form):
+        #일단 임시로 nickname,message,image 들을 저장
+        temp_profile = form.save(commit=False)
+        # 유저라는 데이터가 아직 없어 temp_profile의 유저를 요청하는 유저로 설정
+        temp_profile.user = self.request.user
+        # 다 저장함
+        temp_profile.save()
+        return super().form_valid(form)
