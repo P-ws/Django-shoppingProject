1b2a500b6ab1385ce94207f6b26eef2e14cfab12
P-ws <pws12232002@gmail.com>
2021-08-01 21:42:35 +0900

    <profile update만들기, profile 들을 mypage에 보이게 설정하기> 0. mypage에 수정하는 링크 걸어줌(update), 그리고 내가 올린 프로필 이미지도 보이도록 이미지 링크도 설정, 메세지, 닉네임 보이도록 설정(이미지가 보이려면 프로젝트 urls에 들어가 +static로 media관련 설정) 1. ProfileUpdateView 만들어 설정 (업데이트는 해당 유저만 가능하도록 데코레이터 설정) 2. account에서 만든 데코레이터 복사하여 조금 수정후 사용하기 3. view만들었으니 url설정 update임으로 pk값 받아주는 url 만들기 4. view템플릿에 맞게 update.html생성(마찬가지로 이미지 받아와야함으로 enctype을 설정) 5.

--- profileapp/views.py
+++ profileapp/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView, UpdateView
 
+from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
@@ -24,3 +26,14 @@ class ProfileCreateView(CreateView):
         # 다 저장함
         temp_profile.save()
         return super().form_valid(form)
+
+# 로그인 데코레이터는 왜안한지 모르겟지만, update니까 마찬가지로 해당 유저일때만 변경가능하게 그에조건에맞는 데코레이터 이용
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:base')
+    template_name = 'profileapp/update.html'
+
