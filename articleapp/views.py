98c419094b173a3f35cbdb8a60d570e59b51d115
P-ws <pws12232002@gmail.com>
2021-08-04 16:01:04 +0900

    < article에서 create해서 보여지는 detail에 사진 밑에 comment를 남겨주도록 할려고 commentapp을 만들어 이어줌>
    1. startapp으로 commentapp만들어줌(setting, urls 프로젝트에서 진행)
    2. comment model 만들어주기, forms 만들어주기, view만들어주기
    3. urls 설정 해주기(comment)
    4. create.html만들기 인증되어있다면 폼나오고 안되어있다면 로그인 나오게 그리고 hidden타입 넣는데 잘모르겟음..
    5. article detail에 include로 comment create 인클루드
    6. article detailView에 FormMixin 넣어 폼 넣어주기

--- articleapp/views.py
+++ articleapp/views.py
@@ -5,11 +5,12 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
+from django.views.generic.edit import FormMixin
 
 from articleapp.decorators import article_ownership_required
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
-
+from commentapp.forms import CommentCreationForm
 
 
 @method_decorator(login_required, 'get')
@@ -29,8 +30,9 @@ class ArticleCreateView(CreateView):
         return reverse('articleapp:detail',kwargs={'pk':self.object.pk})
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(DetailView, FormMixin):
     model = Article
+    form_class = CommentCreationForm
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
 
@@ -54,9 +56,8 @@ class ArticleDeleteView(DeleteView):
     success_url = reverse_lazy('articleapp:list')
     template_name = 'articleapp/delete.html'
 
-
 class ArticleListView(ListView):
     model = Article
     context_object_name = 'article_list'
     template_name = 'articleapp/list.html'
-    paginate_by = 1
+    paginate_by = 2
