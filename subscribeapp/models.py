6b0b2eb7bdd6f19e0a6fa9219c92a0505da9f751
P-ws <pws12232002@gmail.com>
2021-08-20 14:43:50 +0900

    구독 모델만들기(폼은 필요없음)

--- subscribeapp/models.py
+++ subscribeapp/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subscription', null=False)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='subscription', null=False)
+
+    # user가 구독을 한번만 누를수있도록 엮어주기
+    class Meta:
+        unique_together = ['user', 'project']
+
+    # 여기서 받아 저장하는 정보가없어서 따로 form은 안만들어준다.
