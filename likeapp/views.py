5d3cd551c14b6a51933d26f9daac887bed9f2034
P-ws <pws12232002@gmail.com>
2021-08-25 14:37:43 +0900

    오류수정

--- likeapp/views.py
+++ likeapp/views.py
@@ -4,7 +4,6 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.template.context_processors import request
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import RedirectView
@@ -12,14 +11,15 @@ from django.views.generic import RedirectView
 from articleapp.models import Article
 from likeapp.models import LikeRecord
 
+
 @method_decorator(login_required, 'get')
 class LikeArticleView(RedirectView):
-
-    def get(self, requset, *args, **kwargs):
-        user = requset.user
+    def get(self, request, *args, **kwargs):
+        user = request.user
         article = Article.objects.get(pk=kwargs['article_pk'])
 
-        like_record = LikeRecord.objects.filter(user=user, article=article)
+        like_record = LikeRecord.objects.filter(user=user,
+                                                article=article)
 
         if like_record.exists():
             # 좋아요가 반영 X
@@ -29,11 +29,11 @@ class LikeArticleView(RedirectView):
             # 좋아요가 반영 O
             LikeRecord(user=user, article=article).save()
 
-        article.like +=1
+        article.like += 1
         article.save()
         messages.add_message(request, messages.SUCCESS, '좋아요가 반영되었습니다')
 
-        return super().get(requset, *args, **kwargs)
+        return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
         return reverse('articleapp:detail', kwargs={'pk': kwargs['article_pk']})
